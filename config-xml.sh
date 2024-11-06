e6b005cad778575ba3992dc19ced49436c744fdd
cmdviegas <cmdviegas@users.noreply.github.com>
2024-11-06 12:06:41 -0300

    Minor improvements

--- config-xml.sh
+++ config-xml.sh
@@ -28,7 +28,7 @@ function update_xml_values() {
  }
 
 # Update core-site.xml, yarn-site.xml, mapred-site.xml, hdfs-site.xml
-update_xml_values "hadoop.http.staticuser.user" "${SYS_USERNAME}" "${HADOOP_CONF_DIR}/core-site.xml"
+update_xml_values "hadoop.http.staticuser.user" "${USERNAME}" "${HADOOP_CONF_DIR}/core-site.xml"
 update_xml_values "yarn.nodemanager.resource.memory-mb" "${MEM_RM}" "${HADOOP_CONF_DIR}/yarn-site.xml"
 update_xml_values "yarn.scheduler.maximum-allocation-mb" "${MEM_MAX}" "${HADOOP_CONF_DIR}/yarn-site.xml"
 update_xml_values "yarn.scheduler.minimum-allocation-mb" "${MEM_MIN}" "${HADOOP_CONF_DIR}/yarn-site.xml"
@@ -36,7 +36,7 @@ update_xml_values "yarn.scheduler.capacity.maximum-am-resource-percent" "${MAX_S
 update_xml_values "yarn.app.mapreduce.am.resource.mb" "${MEM_AM}" "${HADOOP_CONF_DIR}/mapred-site.xml"
 update_xml_values "mapreduce.map.memory.mb" "${MEM_MAP}" "${HADOOP_CONF_DIR}/mapred-site.xml"
 update_xml_values "mapreduce.reduce.memory.mb" "${MEM_RED}" "${HADOOP_CONF_DIR}/mapred-site.xml"
-update_xml_values "dfs.namenode.name.dir" "\/home\/${SYS_USERNAME}\/hdfs-data\/nameNode" "${HADOOP_CONF_DIR}/hdfs-site.xml"
-update_xml_values "dfs.datanode.data.dir" "\/home\/${SYS_USERNAME}\/hdfs-data\/dataNode" "${HADOOP_CONF_DIR}/hdfs-site.xml"
+update_xml_values "dfs.namenode.name.dir" "\/home\/${USERNAME}\/hdfs-data\/nameNode" "${HADOOP_CONF_DIR}/hdfs-site.xml"
+update_xml_values "dfs.datanode.data.dir" "\/home\/${USERNAME}\/hdfs-data\/dataNode" "${HADOOP_CONF_DIR}/hdfs-site.xml"
 [ "$NODE_REPLICAS" -ge 2 ] && update_xml_values "dfs.replication" "2" "${HADOOP_CONF_DIR}/hdfs-site.xml"
 
